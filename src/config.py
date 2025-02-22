2322b7b4bac25ed10e183e2bdb59164e2e8845e3
HaykSahakyan0 <hayk.sahakyan@cognaize.com>
2025-02-22 15:24:17 +0400

    add predict.py

--- src/config.py
+++ src/config.py
@@ -20,6 +20,7 @@ def set_seed(seed=42):
 
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
+
 class CONFIG:
     def __init__(self):
         self.base_path = BASE_PATH
@@ -33,14 +34,16 @@ class CONFIG:
         self.train_data = os.path.join(self.data_dir, "train.txt")
         self.test_data = os.path.join(self.data_dir, "test.txt")
 
-
         self.model_dir = os.path.join(BASE_PATH, "models")
+        self.best_model_path = os.path.join(self.model_dir, "best_model.pth")
+        self.gcn_gat_model_path = os.path.join(self.model_dir, "gcn_gat_model.pth")
+        self.seal_model_path = os.path.join(self.model_dir, "seal_model.pth")
+
         self.sprs_v_cls_model_path = os.path.join(BASE_PATH, "models", "sparse_vector_classifier.pth")
 
         self.log_dir = os.path.join(BASE_PATH, "logs")
 
         self.model_name = 'bert-base-uncased'
-
 
         self.train_params = {
             'train_size': 0.85,
@@ -53,23 +56,24 @@ class CONFIG:
             "learning_rate": 0.001,
             "gradient_accumulation_steps": 1,
             'hidden_dim': 16,
+            'input_channels': 932,
             'gcn_params': {
                 'hidden_channels': 256,
                 'out_channels': 2,
                 'dropout': 0.2,
-                'threshold': 0.3,
+                'threshold': 0.5,
                 'lr': 0.01,
                 'weight_decay': 5e-4,
-                'epochs': 50,
+                'epochs': 100,
             },
             'seal_params': {
                 'hidden_channels': 256,
-                'out_channels': 2,
-                'dropout': 0.2,
-                'threshold': 0.3,
-                'lr': 0.01,
+                'out_channels': 64,
+                'dropout': 0.3,
+                'threshold': 0.5,
+                'lr': 0.001,
                 'weight_decay': 5e-4,
-                'epochs': 50,
+                'epochs': 100,
             },
         }
         self.model_params = {
--- src/models/model.py
+++ src/models/model.py
@@ -159,3 +159,127 @@ class SEALModel(nn.Module):
         x = self.bn2(x)
         x = F.leaky_relu(x)
         return self.readout(x, batch)
+
+
+
+
+
+
+
+
+
+
+# class SEALModel(nn.Module):
+#     def __init__(self, in_channels, hidden_channels, out_channels, dropout=0.3):
+#         super(SEALModel, self).__init__()
+#
+#         # SEAL's subgraph embedding approach
+#         self.conv1 = GCNConv(in_channels, hidden_channels)
+#         self.bn1 = BatchNorm(hidden_channels)
+#
+#         self.conv2 = SAGEConv(hidden_channels, hidden_channels // 2)
+#         self.bn2 = BatchNorm(hidden_channels // 2)
+#
+#         self.conv3 = GCNConv(hidden_channels // 2, hidden_channels // 4)
+#         self.bn3 = BatchNorm(hidden_channels // 4)
+#
+#         self.conv4 = DenseGCNConv(hidden_channels // 4, out_channels)
+#         self.dropout = Dropout(p=dropout)
+#
+#         self.readout = global_add_pool  # Readout function for subgraph embeddings
+#         self.mlp = nn.Sequential(
+#             nn.Linear(out_channels, out_channels // 2),
+#             nn.ReLU(),
+#             nn.Linear(out_channels // 2, 1)
+#         )
+#
+#     def forward(self, x, edge_index, batch):
+#         x = self.conv1(x, edge_index)
+#         x = self.bn1(x)
+#         x = F.leaky_relu(x)
+#         x = self.dropout(x)
+#
+#         x = self.conv2(x, edge_index)
+#         x = self.bn2(x)
+#         x = F.leaky_relu(x)
+#         x = self.dropout(x)
+#
+#         x = self.conv3(x, edge_index)
+#         x = self.bn3(x)
+#         x = F.leaky_relu(x)
+#         x = self.dropout(x)
+#
+#         x = self.conv4(x, edge_index)
+#         x = self.readout(x, batch)  # Readout for whole subgraph embedding
+#         x = self.mlp(x)  # Final MLP layer for link prediction
+#         return x
+#
+#     def get_embeddings(self, x, edge_index, batch):
+#         """Extract node embeddings before final layer."""
+#         x = self.conv1(x, edge_index)
+#         x = self.bn1(x)
+#         x = F.leaky_relu(x)
+#         x = self.conv2(x, edge_index)
+#         x = self.bn2(x)
+#         x = F.leaky_relu(x)
+#         return self.readout(x, batch)
+
+# class SEALModel(nn.Module):
+#     def __init__(self, in_channels, hidden_channels, out_channels, dropout=0.3):
+#         super(SEALModel, self).__init__()
+#
+#         # SEAL's subgraph embedding approach
+#         self.conv1 = GCNConv(in_channels, hidden_channels)
+#         self.bn1 = BatchNorm(hidden_channels)
+#
+#         self.conv2 = SAGEConv(hidden_channels, hidden_channels // 2)
+#         self.bn2 = BatchNorm(hidden_channels // 2)
+#
+#         self.conv3 = GCNConv(hidden_channels // 2, hidden_channels // 4)
+#         self.bn3 = BatchNorm(hidden_channels // 4)
+#
+#         self.conv4 = GCNConv(hidden_channels // 4, out_channels)
+#         self.dropout = Dropout(p=dropout)
+#
+#         self.readout = global_add_pool  # Readout function for subgraph embeddings
+#         self.mlp = nn.Sequential(
+#             nn.Linear(out_channels, out_channels // 2),
+#             nn.ReLU(),
+#             nn.Linear(out_channels // 2, 1)
+#         )
+#
+#     def forward(self, x, edge_index, batch):
+#         x = self.conv1(x, edge_index)
+#         x = self.bn1(x)
+#         x = F.leaky_relu(x)
+#         x = self.dropout(x)
+#
+#         x = self.conv2(x, edge_index)
+#         x = self.bn2(x)
+#         x = F.leaky_relu(x)
+#         x = self.dropout(x)
+#
+#         x = self.conv3(x, edge_index)
+#         x = self.bn3(x)
+#         x = F.leaky_relu(x)
+#         x = self.dropout(x)
+#
+#         x = self.conv4(x, edge_index)
+#         x = self.readout(x, batch)  # Readout for whole subgraph embedding
+#         x = self.mlp(x)  # Final MLP layer for link prediction
+#         return x
+#
+#     def get_embeddings(self, x, edge_index, batch):
+#         """Extract node embeddings before final MLP layer."""
+#         x = self.conv1(x, edge_index)
+#         x = self.bn1(x)
+#         x = F.leaky_relu(x)
+#         x = self.conv2(x, edge_index)
+#         x = self.bn2(x)
+#         x = F.leaky_relu(x)
+#         x = self.conv3(x, edge_index)
+#         x = self.bn3(x)
+#         x = F.leaky_relu(x)
+#         x = self.conv4(x, edge_index)
+#         return self.readout(x, batch)
+
